5c62dfa665c3bc2a366e3e975d324aa3474568b1
PatrykKups <pkups@anaconda.com>
2025-11-18 13:03:33 +0100

    eigen 5.1  (#4)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,36 +1,19 @@
-md build
-cd build
-
-set CMAKE_CONFIG="Release"
-
-cmake -LAH -G"Ninja"              ^
-  -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%         ^
+cmake -B build -G"Ninja"              ^
+  -DCMAKE_BUILD_TYPE=Release          ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%      ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%   ^
   -DEIGEN_BUILD_PKGCONFIG=ON ^
-  ..
+  -DEIGEN_BUILD_BLAS=OFF ^
+  -DEIGEN_BUILD_LAPACK=OFF ^
 if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG%
-if errorlevel 1 exit 1
-cmake --install .
+cmake --build build --config Release --target install
 if errorlevel 1 exit 1
 
 rem Just make the basic tests as all the tests take too long to run.
 FOR /L %%A IN (1,1,8) DO (
-  cmake --build . --config %CMAKE_CONFIG% --target basicstuff_%%A
+  cmake --build build --config Release --target basicstuff_%%A
 )
 if errorlevel 1 exit 1
-ctest -R basicstuff*
+ctest --test-dir build -R basicstuff* -V
 if errorlevel 1 exit 1
-goto :eof
-
-:TRIM
-  SetLocal EnableDelayedExpansion
-  Call :TRIMSUB %%%1%%
-  EndLocal & set %1=%tempvar%
-  GOTO :eof
-
-  :TRIMSUB
-  set tempvar=%*
-  GOTO :eof
--- recipe/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-test -d ${PREFIX}/include/eigen3
-
-${CXX} -I${PREFIX}/include -o test test.cc
-
-./test
